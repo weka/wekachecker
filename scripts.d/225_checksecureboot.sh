dbd21e7ce4caa2d7701b5d540d2731b67f00b596
James Vaughn <james.vaughn@weka.io>
2022-11-28 16:20:34 +0000

    Handle additional output in SecureBoot check
    mokutil can actually print messages such as:
    
      SecureBoot disabled
      Platform is in Setup Mode
    
    Update script to handle this.

--- scripts.d/225_checksecureboot.sh
+++ scripts.d/225_checksecureboot.sh
@@ -9,17 +9,15 @@ if ! [ -d /sys/firmware/efi/ ]; then
 	exit 0
 fi
 
-if ! which mokutil; then
+if ! sb_state=$(mokutil --sb-state); then
 	write_log 'mokutil not found, unable to determine Secure Boot status'
 	exit 254
 fi
 
-sb_state=$(mokutil --sb-state)
-
-if [ "$sb_state" = 'SecureBoot disabled' ]; then
+if [[ $sb_state = SecureBoot' 'disabled* ]]; then
 	write_log 'Secure Boot disabled'
 	exit 0
-elif [ "$sb_state" = 'SecureBoot enabled' ]; then
+elif [[ $sb_state = SecureBoot' 'enabled* ]]; then
 	write_log 'Secure Boot enabled; disable in the BIOS/UEFI interface'
 	exit 254
 else
