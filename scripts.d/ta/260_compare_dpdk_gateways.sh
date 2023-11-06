d32fdc6bad0e6e4765b6275a9949185c3d79f780
Jack Challen <jack.challen@weka.io>
2023-11-06 12:56:42 +0000

    Null commit: whitespace

--- scripts.d/ta/260_compare_dpdk_gateways.sh
+++ scripts.d/ta/260_compare_dpdk_gateways.sh
@@ -15,6 +15,3 @@ for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header); do
     sudo weka local resources --container ${WEKA_CONTAINER} --json | jq -r "[.net_devices[].gateway] | .[]" | sort -n
 done
 ) | sha256sum | awk '{print $1}'
-
-
-
