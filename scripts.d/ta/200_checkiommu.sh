0672ffbaa36271aa71f2e3aaad2ec97999089b9f
Jack Challen <jack.challen@weka.io>
2023-11-06 09:18:48 +0000

    Improve shell script syntax, add version-specific checks, add links to Jira

--- scripts.d/ta/200_checkiommu.sh
+++ scripts.d/ta/200_checkiommu.sh
@@ -2,27 +2,39 @@
 
 DESCRIPTION="Check for IOMMU disabled"
 SCRIPT_TYPE="parallel"
+JIRA_REFERENCE="WEKAPP-323895"
+WTA_REFERENCE=""
+RETURN_CODE=0
+MIN_VERSION="4.1.2"
+MAX_VERSION="4.2.2"
 
+# Use core-util's sort -V to dermine if version $1 is <= version $2
+verlte() {
+    [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+}
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
 
+WEKA_VERSION=$(weka version current)
 
-#find /sys | grep dmar &> /dev/null
-#if [ $? == 0 ]; then    # found a dmar, which we don't want
-#    echo "ERROR: IOMMU is enabled on `hostname`"
-#    exit "1"
-#else
-#    echo "IOMMU disabled"
-#fi
+IOMMUCLASS=$(ls /sys/class/iommu | wc -l)
+IOMMUGROUPS=$(ls /sys/kernel/iommu_groups | wc -l)
 
-iommuclass=`ls /sys/class/iommu | wc -l`
-iommugroups=`ls /sys/kernel/iommu_groups | wc -l`
-if [ $iommuclass -eq "0" ] && [ $iommugroups -eq "0" ]; then    # check for iommu devices
-    echo "IOMMU not configured on `hostname`"
+if [ $IOMMUCLASS -eq "0" ] && [ $IOMMUGROUPS -eq "0" ]; then    # check for iommu devices
+    echo "IOMMU not configured on $(hostname)"
     ret="0"
 else
-    echo "IOMMU configured on `hostname` - should be disabled"
-    ret="1"
+    if verlte ${MIN_VERSION} ${WEKA_VERSION} && verlte ${WEKA_VERSION} ${MAX_VERSION} ; then
+        echo "This version is classified as a susceptible version, and"
+    fi
+    echo "IOMMU is configured on $(hostname) - this should be disabled - refer"
+    if [[ ! -z "${WTA_REFERENCE}" ]]; then
+        echo "to ${JIRA_REFERENCE}, discussed in ${WTA_REFERENCE}"
+    else
+        echo "to ${JIRA_REFERENCE}"                                                                                                   
+    fi
+    RETURN_CODE=1
 fi
 
-exit $ret
-
-
+exit ${RETURN_CODE}
