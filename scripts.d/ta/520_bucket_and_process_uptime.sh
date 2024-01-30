787e3f4be68b517cca65df60ccd63ef45537edc2
Jack Challen <jack.challen@weka.io>
2024-01-30 17:51:17 +0000

    Basic check to verify bucket and process uptime

---
+++ scripts.d/ta/520_bucket_and_process_uptime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash                                                                                                                                                                                                                         
+
+#set -ue # Fail with an error code if there's any sub-command/variable error
+
+DESCRIPTION="Check if buckets have been rebooting"
+SCRIPT_TYPE="single"
+JIRA_REFERENCE=""
+WTA_REFERENCE=""
+KB_REFERENCE=""
+RETURN_CODE=0
+
+MOST_RECENT_BUCKET_STARTTIME=$( date +%s --date=$(weka cluster bucket  --json -s uptime | jq -cr '.|last|.up_since'))
+MOST_RECENT_PROCESS_STARTTIME=$(date +%s --date=$(weka cluster process --json -s uptime | jq -cr '.|last|.up_since'))
+CURRENT_TIME=$(                 date +%s)
+
+if [[ $((${CURRENT_TIME}-${MOST_RECENT_BUCKET_STARTTIME})) -lt 3600 ]]; then
+    RETURN_CODE="254"
+    echo "Weka buckets have been restarted within the last hour. This may not be a problem on a new cluster"
+    echo "but could be indicative of problems (e.g. network flapping"
+fi
+if [[ $((${CURRENT_TIME}-${MOST_RECENT_PROCESS_STARTTIME})) -lt 3600 ]]; then
+    RETURN_CODE="254"
+    echo "Weka processes have been restarted within the last hour. This may not be a problem on a new cluster"
+    echo "but could be indicative of problems (e.g. network flapping"
+fi
+
+exit ${RETURN_CODE}
