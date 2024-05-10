870108f2f7b04b665ea4523f3f69526fdd467a04
vrragosta <vincent.ragosta@weka.io>
2024-05-10 15:21:37 -0400

    Update 500_sysctl_rp_filter.sh
    Add echo for success condition.

--- scripts.d/ta/500_sysctl_rp_filter.sh
+++ scripts.d/ta/500_sysctl_rp_filter.sh
@@ -42,6 +42,8 @@ if [[ $RP_FILTER_VALUE_ALL != "2" ]]; then
       echo "This can disrupt floating IP addresses for protocols."
       echo "It is recommended to set net.ipv4.conf.${INTERFACE}.rp_filter or net.ipv4.conf.all.rp_filter to 2."
     fi
+    else
+      echo "net.ipv4.conf.all.rp_filter is set to 2, no further testing necessary."
   done
 fi
 
