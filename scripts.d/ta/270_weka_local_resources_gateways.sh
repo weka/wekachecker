4bbd7b6fb7469208b72046f2a2ecbb0d02266f1d
vrragosta <vincent.ragosta@weka.io>
2024-09-18 11:31:51 -0400

    Local resources gateway update
    - Exclude protocol containers
    - Remove dependency on python
    - Skip interfaces that begin with ib

--- scripts.d/ta/270_weka_local_resources_gateways.sh
+++ scripts.d/ta/270_weka_local_resources_gateways.sh
@@ -8,34 +8,29 @@ SCRIPT_TYPE="parallel"
 
 RETURN_CODE=0
 
-for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header); do
-    if [[ ( ${WEKA_CONTAINER} == "ganesha" ) || \
-          ( ${WEKA_CONTAINER} == "samba" )   || \
-          ( ${WEKA_CONTAINER} == "smb"   ) ]] ; then
-        continue
-    fi
-    # Look for network devices with no gateway
-    RESOURCES=$(sudo weka local resources --container ${WEKA_CONTAINER} --json)
-    NUMBER_OF_DEVICES_WITH_NO_GATEWAY=$(echo ${RESOURCES} | python3 -c 'import sys, json; data = json.load(sys.stdin); print(len([device for device in data["net_devices"] if device["gateway"] == ""]))')
-
-
-    if [[ ${NUMBER_OF_DEVICES_WITH_NO_GATEWAY} -ne 0 ]] ; then
-        DEVICES_WITH_NO_GATEWAY=$(echo ${RESOURCES} | python3 -c 'import sys, json; data = json.load(sys.stdin); print("\n".join([device["name"] for device in data["net_devices"] if device["gateway"] == ""]))')
-        echo "The container ${WEKA_CONTAINER} has the following network devices defined without an IP"
-        echo "gateway - this might not be a mistake but means Weka POSIX traffic will not"
-        echo "leave this subnet"
-        echo ""
-        echo ${DEVICES_WITH_NO_GATEWAY}
-        echo ""
+for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header | grep -vw -e envoy -e ganesha -e samba -e smbw -e s3); do
+    DEVICES_WITH_NO_GATEWAY=""
+    # skip network devices beginning with ib, as a weak attempt at identifying infiniband interfaces
+    for NET_DEVICE in $(weka local resources net -C ${WEKA_CONTAINER} --stable | awk 'NR>1{print $1}' | grep -v -e ^ib); do
+        if [[ -n ${NET_DEVICE} ]]; then
+            if weka local resources net -C drives0 -J --stable | grep -A 1 -w ${NET_DEVICE} | grep gateway | grep -q \"\"; then
+                DEVICES_WITH_NO_GATEWAY="${DEVICES_WITH_NO_GATEWAY} ${NET_DEVICE} "
+            fi
+        fi
+    done
+    if [[ -n ${DEVICES_WITH_NO_GATEWAY} ]]; then
+        echo "The container ${WEKA_CONTAINER} has the network devices ${DEVICES_WITH_NO_GATEWAY}"
+        echo "defined without an IP gateway - this might not be a mistake but means Weka"
+        echo "POSIX traffic will not leave this subnet."
         echo "The likely fix for this is to do weka local resources net remove for each device,"
         echo "then add back in with weka local resource net add <DEVNAME> --gateway ... --netmask .."
-        #RETURN_CODE=254
-        exit 254
-    fi 
+        echo
+        RETURN_CODE=254
+    fi
 done
+
 if [[ ${RETURN_CODE} -eq 0 ]]; then
     echo "All Weka containers have network devices with gateways"
 fi
+
 exit ${RETURN_CODE}
-
-
