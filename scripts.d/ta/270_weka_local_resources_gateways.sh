9ce7a6d2bee1cff06925a093630ac8d315e0d058
vrragosta <vincent.ragosta@weka.io>
2024-09-25 13:18:00 -0400

    Update gateway check
    - Parse json output for better consistency.

--- scripts.d/ta/270_weka_local_resources_gateways.sh
+++ scripts.d/ta/270_weka_local_resources_gateways.sh
@@ -10,19 +10,27 @@ RETURN_CODE=0
 
 for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header | grep -vw -e envoy -e ganesha -e samba -e smbw -e s3); do
     DEVICES_WITH_NO_GATEWAY=""
-    # skip network devices beginning with ib, as a weak attempt at identifying infiniband interfaces
-    for NET_DEVICE in $(weka local resources net -C ${WEKA_CONTAINER} --stable | awk 'NR>1{print $1}'); do
+    NET_DEVICE=""
+    NET_GATEWAY=""
+
+    while read NET_ENTRY; do
+        if [[ ${NET_ENTRY} =~ "gateway:"(.*)"name:"(.*) ]]; then
+            NET_GATEWAY=${BASH_REMATCH[1]}
+            NET_DEVICE=${BASH_REMATCH[2]}
+        fi
+
         if [[ -n ${NET_DEVICE} ]]; then
             if [[ -d /sys/class/net/${NET_DEVICE} ]]; then
                 NET_TYPE=$(cat /sys/class/net/${NET_DEVICE}/type)
                 if [[ -n ${NET_TYPE} && ${NET_TYPE} == "1" ]]; then # Only check ethernet devices
-                    if weka local resources net -C drives0 -J --stable | grep -A 1 -w ${NET_DEVICE} | grep gateway | grep -q \"\"; then
+                    if [[ -z ${NET_GATEWAY} ]]; then
                         DEVICES_WITH_NO_GATEWAY="${DEVICES_WITH_NO_GATEWAY}${NET_DEVICE} "
                     fi
                 fi
             fi
         fi
-    done
+    done < <(weka local resources -C ${WEKA_CONTAINER} net --stable -J | grep -w -e gateway -e name | paste - - | tr -d \"\,[:blank:])
+
     if [[ -n ${DEVICES_WITH_NO_GATEWAY} ]]; then
         echo "The container ${WEKA_CONTAINER} has the network device(s) ${DEVICES_WITH_NO_GATEWAY}"
         echo "defined without an IP gateway - this might not be a mistake but means Weka"
