6bb792c89020b27be1f51ea29571b8530de2132c
vrragosta <vincent.ragosta@weka.io>
2024-11-05 13:54:29 -0500

    Dataplane mgmt ip check
    Additionally validate the count of management IPs

--- scripts.d/ta/825_ha_mgmt_ip.sh
+++ scripts.d/ta/825_ha_mgmt_ip.sh
@@ -10,13 +10,20 @@ RETURN_CODE=0
 
 for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header | grep -e compute -e drives -e frontend); do
     NET_NAME=""
+    NET_COUNT=0
+    SINGLE_IP=0
 
     while read NET_ENTRY; do
         if [[ ${NET_ENTRY} =~ "name:"(.*) ]]; then
             NET_NAME=${BASH_REMATCH[1]}
         fi
+        if [[ ${NET_ENTRY} =~ "ips:[]" ]]; then
+            SINGLE_IP=1
+        fi
 
         if [[ -n ${NET_NAME} ]]; then
+            NET_COUNT=$((NET_COUNT+1))
+
             if [[ $(ip -4 -j -o addr show dev ${NET_NAME} 2>/dev/null | tr -d \"\[:blank:]) =~ "local:"([0-9\.]+) ]]; then
                 NET_IP=${BASH_REMATCH[1]}
                 MATCH_FOUND=0
@@ -32,14 +39,25 @@ for WEKA_CONTAINER in $(sudo weka local ps --output name --no-header | grep -e c
                 fi
             fi
         fi
-    done < <(weka local resources -C ${WEKA_CONTAINER} net --stable -J | grep -w -e name | tr -d \"\,[:blank:])
+    done < <(weka local resources -C ${WEKA_CONTAINER} net --stable -J | grep -w -e name -e ips | paste - - | tr -d \"\,[:blank:])
+
+    MGMT_IP_COUNT=$(weka local resources -C ${WEKA_CONTAINER} --stable | grep -e ^"Management IPs" | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" | wc -l)
+    if [[ ${SINGLE_IP} -eq 1 ]] && [[ ${NET_COUNT} -ne ${MGMT_IP_COUNT} ]]; then
+        echo "WARN: Container ${WEKA_CONTAINER} has ${NET_COUNT} dataplane NICs, but ${MGMT_IP_COUNT} management IPs"
+        RETURN_CODE=254
+
+    # Ostensibly this should only occur in cloud environments?
+    elif [[ ${SINGLE_IP} -eq 0 ]] && [[ ${MGMT_IP_COUNT} -gt 1 ]]; then
+        echo "WARN: Container ${WEKA_CONTAINER} has ${MGMT_IP_COUNT} management IPs"
+        RETURN_CODE=254
+    fi
 done
 
 if [[ ${RETURN_CODE} -eq 0 ]]; then
     echo "All Weka dataplane NICs have valid management IPs"
 else
     echo "Recommended Resolution: assign an appropriate set of management ips for each container."
-    echo "Minimally, there should be one management IP per dataplane NIC. Management IPs can be set"
+    echo "There should be one management IP per dataplane NIC. Management IPs can be set"
     echo "by running the following command"
     echo " weka local resources --container <WEKA-CONTAINER> management-ips <IP1> <IP2>"
 fi
