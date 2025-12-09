d26fbcbe2fd3efff358fd4b240e0cbd92817d854
Jack Challen <jack.challen@weka.io>
2025-12-09 13:54:30 +0000

    Check for extended APIC availability following customer incident
    A customer saw vector space exhaustion, which meant NVMEs became
    unusable. This was caused by x2apic not being available, (which
    can itself by caused by IOMMU being disabled)
    
    Therefore, check that x2apic (Intel) or ext_apic (AMD) is available,
    which should result in plenty of IRQ space.
    
    https://weka-support.slack.com/archives/C066DNGSAE5/p1764947984669029

---
+++ scripts.d/ta/415_check_extended_apic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#set -ue # Fail with an error code if there's any sub-command/variable error
+
+DESCRIPTION="Check that extended APIC is available for assigning IRQs"
+# script type is single, parallel, sequential, or parallel-compare-backends
+SCRIPT_TYPE="parallel"
+JIRA_REFERENCE=""
+WTA_REFERENCE=""
+KB_REFERENCE=""
+
+RETURN_CODE="0"
+
+#check that extended APIC (or x2apic) is available, because it's required for more
+# space for IRQs
+
+if (grep -m1 -q -E '^flags.*(\<extapic|\<x2apic)' /proc/cpuinfo) ; then
+else
+    RETURN_CODE="254"
+    echo "There is no extended APIC available. This can prevent the assignment"
+    echo "of enough IRQs to support all hardware, resulting in the kernel"
+    echo "error message: vector space exhaustion"
+    echo "A frequent cause of no extended APIC is the disabling of IOMMUs"
+fi
+
+if [[ ${RETURN_CODE} -eq "0" ]]; then
+    echo "Extended APIC reports available"
+else
+    echo "No extended APIC available"
+fi
+exit ${RETURN_CODE}
